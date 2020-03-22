9ea3acafbc4f4dfd465c90a2c622e9850723afbc
Arihant Daga <arihant.ekism@gmail.com>
2020-03-22 19:14:15 +0530

    Added Airtable Suport

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const dotenv = require("dotenv").config();
+const Airtable = require('airtable');
 const fs = require('fs');
+let airtablebase;
+
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+    return { hello: 'world' }
 });
 
-fastify.post('/data', async (request, reply)=>{
-    fs.writeFile('./data.txt', data, (err)=>{
-        if(err){
-            throw err;
-        }
-        return {success: 1};
-    });
-})
+fastify.post("/data", async (request, reply) => {
+    const data = {
+        otp: request.body.otp,
+        sender: request.body.sender,
+        message: request.body.message,
+    }
+    console.log(data);
+    return await new Promise((res, rej) => {
+        airtablebase('OtpCollector').create([
+            {
+                "fields": data
+            }], (err, records) => {
+                if (err) {
+                    return rej(err)
+                }
+                return res({ success: 1 })
+            });
+    })
+
+});
 
 
 // Run the server!
 const start = async () => {
-  try {
-    await fastify.listen(process.env.PORT  || 3000, '0.0.0.0')
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+    try {
+        await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+
+        Airtable.configure({ apiKey: process.env.AIRTABLE_KEY });
+        airtablebase = Airtable.base(process.env.AIRTABLE_BASE)
+
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
 }
 start()
